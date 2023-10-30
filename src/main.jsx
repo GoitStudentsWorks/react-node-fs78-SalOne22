7e6039720a7d68c93b479638d22e7ae839dcb3db
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-31 00:52:59 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import './globals.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={<ScreenLoader />} persistor={persistor}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename="/react-node-fs78-SalOne22">
         <App />
       </BrowserRouter>
     </PersistGate>
